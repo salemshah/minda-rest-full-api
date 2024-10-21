f51337ca946b5cce864aa5a339dd017350dd7d90
Salem Shah <salemshahdev@gmail.com>
2024-10-22 01:04:58 +0200

    MAD-37: feat: Set Up the Set Up eslint, prettier, nodemon, commitlint and ...

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^(MAD-\d+):\s(?<type>\w+):\s(?<subject>.+)$/,
+            headerCorrespondence: ['jira', 'type', 'subject'],
+        },
+    },
+    rules: {
+        'subject-case': [0, 'never'], // Disable subject case enforcement
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert'],
+        ],
+        'header-max-length': [2, 'always', 72],
+    },
+};
