633e056b02d9d5cd756c456ecde15793e617d015
Salem Shah <salemshahdev@gmail.com>
2024-10-23 17:10:38 +0200

    MAD-69: fix: Enhance CI workflow with Prisma and linting steps 2

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -1,54 +1,55 @@
 import 'reflect-metadata';
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import initializeApp from '../app';
 import dotenv from 'dotenv';
-import {execSync} from 'child_process';
-import {Application} from 'express';
+import { execSync } from 'child_process';
+import { Application } from 'express';
 
 // Load environment variables for testing
-dotenv.config({path: '.env.test'});
+dotenv.config({ path: '.env.test' });
 
 // Mock External Dependencies
 jest.mock('../utils/sendEmail', () => ({
-    sendEmail: jest.fn().mockResolvedValue(true),
+  sendEmail: jest.fn().mockResolvedValue(true),
 }));
 
-jest.mock('../utils/logger', () => ({
-    info: jest.fn(),
-    error: jest.fn(),
-    warn: jest.fn(),
-    debug: jest.fn(),
-}));
+// jest.mock('../utils/logger', () => ({
+//     info: jest.fn(),
+//     error: jest.fn(),
+//     warn: jest.fn(),
+//     debug: jest.fn(),
+// }));
 // Initialize Prisma Client
 const prisma = new PrismaClient();
 
 // Function to run Prisma migrations
 const runMigrations = () => {
-    try {
-        execSync('npx prisma migrate deploy', {stdio: 'inherit'});
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    execSync('npx prisma migrate deploy', { stdio: 'inherit' });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 let app: Application;
 
 // Before all tests, run migrations, connect to the database, and initialize the app
 beforeAll(async () => {
-    runMigrations();
-    await prisma.$connect();
-    await prisma.parent.deleteMany();
-    app = await initializeApp();
+  runMigrations();
+  await prisma.$connect();
+  await prisma.parent.deleteMany();
+  console.log('All deleted from parent');
+  app = await initializeApp();
 });
 
 // After all tests, disconnect from the database
 afterAll(async () => {
-    await prisma.$disconnect();
+  await prisma.$disconnect();
 });
 
 // After each test, reset mocks to ensure they don't interfere with other tests
 afterEach(() => {
-    jest.clearAllMocks()
+  jest.clearAllMocks();
 });
 
-export {app};
+export { app };
