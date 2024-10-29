280fc183d35fbbefabe781ab6a93fb6a14185757
Salem Shah <salemshahdev@gmail.com>
2024-10-29 23:02:49 +0100

    MAD-70: fix: dockrize & ci/cd & nginx

--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -26,4 +26,10 @@ export default function expressLoader(app: Application): void {
 
   // API Routes
   app.use('/api', routes);
+
+  app.get('/test-app', (req, res) => {
+    res.send({
+      message: 'Success message, APP is ok',
+    });
+  });
 }
